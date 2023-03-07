c77337041a3b0c04de96e22a05da9feb9177af0d
kodenik <nikhilb@koderoom.com>
2023-03-06 20:23:34 -0800

    0-get_list_students.js

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 function getListStudents() {
   const obj = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'james', location: 'Columbia' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 
